1f1a65aec338b938804f85a5f4908a4326740e4b
Dvir Avrahami <dviravr@gmail.com>
2021-12-01 14:07:39 +0200

    work with firebase

--- src/app/core/models/patient.model.ts
+++ src/app/core/models/patient.model.ts
@@ -1,5 +1,4 @@
 import { BaseModel } from './base.model';
-import { Therapist } from './therapist.model';
 import { DocumentReference } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
 
